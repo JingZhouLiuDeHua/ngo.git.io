c536aeed65134722c6b711c8aa92480d49c97af0
JingZhouLiuDeHua <yyyttt2016@outlook.com>
2019-09-24 16:10:36 -0400

    first API

--- Order/views.py
+++ Order/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Order
+from rest_framework import viewsets
+from .serializers import OrderSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Order.objects.all().order_by('-date_joined')
+    serializer_class = OrderSerializer
